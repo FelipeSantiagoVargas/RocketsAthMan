33bdb28142ab7486b3bee782392e2956b51677fb
eithri <guillermo.paez@uptc.edu.co>
2021-08-02 17:27:35 -0500

    installed tailwindcss

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className=" bg-gradient-to-r from-yellow-400 to-pink-500">
+      <h1>TE AMOOOOOOOOOOOOOOOOOOO</h1>
+    </div>  
+  );
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    red: {
+      light: "#85d7ff",
+      DEFAULT: "#1fb6ff",
+      dark: "#009eeb",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
