e976aef95453652e905746e95467ee4075c53241
eithri <guillermo.paez@uptc.edu.co>
2021-08-09 12:10:26 -0500

    added header main page

--- frontend/src/pages/home.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return (
-    <div className=" bg-gray">
-      <h1>TE AMOOOOOOOOOOOOOOOOOOO</h1>
-    </div>  
-  );
-}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,15 +2,20 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      white: "#ffffff",
-      gray: "#E5E5E5",
-      red: {
-        light: "#DC1818",
-        DEFAULT: "#C61414",
-        dark: "#950F0F",
+    extend: {
+      colors: {
+        white: "#ffffff",
+        gray: "#E5E5E5",
+        red: {
+          light: "#DC1818",
+          DEFAULT: "#C61414",
+          dark: "#950F0F",
+        },
+        brown: "#130202",
       },
-      brown: "130202",
+      fontFamily: {
+        body: ["Quicksand"],
+      },
     },
   },
   variants: {
