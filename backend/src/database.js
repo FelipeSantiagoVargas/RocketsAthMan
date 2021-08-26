200252925c9ff5d6c3fb82d939ca89a8fab128ff
FelipeSantiagoVargas <felipeesantii@gmail.com>
2021-08-25 23:40:27 -0500

    send mail ready

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,12 @@
+require('dotenv').config()
 import mongoose from 'mongoose'
 
-mongoose.connect("mongodb://localhost/rockets",{
+const {MONGO_DB_URI,MONGO_DB_URI_TEST,NODE_ENV} = process.env
+
+const connection = NODE_ENV === 'test' ? MONGO_DB_URI_TEST: MONGO_DB_URI
+console.log(connection)
+
+mongoose.connect(connection,{
     useNewUrlParser: true ,
     useUnifiedTopology: true,
     useCreateIndex: true
