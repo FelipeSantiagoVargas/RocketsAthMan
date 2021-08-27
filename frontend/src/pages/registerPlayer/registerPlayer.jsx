9e8980589df93969bff9958642aab326344550f7
Juan Camilo Sepúlveda Fajardo <juan.kmilo11@hotmail.com>
2021-08-27 12:47:39 -0500

    added post method

--- frontend/src/pages/registerPlayer/registerPlayer.jsx
+++ frontend/src/pages/registerPlayer/registerPlayer.jsx
@@ -1,22 +1,27 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
 
 import "./registerPlayer.css";
 
+const url = "http://3.238.91.249:4000/api/players"
+
 export default class registerPlayer extends Component {
+
+
 
   state = {
     form: {
-      "imgUrl": "Felipe",
+      "imgUrl": "unaimagen.jpg",
       name: '',
       lastName: '',
-      "gender": '',
+      gender: '',
       birthday: '',
       documentId: '',
       phone: '',
       address: '',
-      "position": '',
+      position: '',
       height: '',
       weight: '',
       eps: '',
@@ -35,6 +40,13 @@ export default class registerPlayer extends Component {
     console.log(this.state.form);
   }
 
+  postPlayers() {
+    axios.post(url, this.state.form).then(response => {
+    }).catch(error => {
+      console.log(error.message);
+    })
+  }
+
   render() {
     return (
       <div className="w-full min-h-screen pl-20 pr-20 bg-auto object-fill back-image" >
@@ -48,21 +60,21 @@ export default class registerPlayer extends Component {
             <input class="bg-white border-2 border-black rounded py-2 px-4 placeholder-gray-500 text-black text-lg focus:bg-red-50 " type="text" placeholder="Teléfono" name="phone" onChange={this.handleChange} required />
             <input class="bg-white border-2 border-black rounded py-2 px-4 placeholder-gray-500 text-black text-lg focus:bg-red-50 " type="text" placeholder="Apellido" name="lastname" onChange={this.handleChange} required />
             <input class="bg-white border-2 border-black rounded py-2 px-4 placeholder-gray-500 text-black text-lg focus:bg-red-50 " type="text" placeholder="Dirección" name="address" onChange={this.handleChange} required />
-            <select defaultValue={'DEFAULT'} class="bg-white border-2 border-black rounded py-2 px-4 placeholder-gray-500 text-black text-lg focus:bg-red-50">
+            <select name="gender" defaultValue={"DEFAULT"} onChange={this.handleChange} class="bg-white border-2 border-black rounded py-2 px-4 placeholder-gray-500 text-black text-lg focus:bg-red-50">
               <option value="DEFAULT" disabled defaultValue>Género: </option>
-              <option value="1">Hombre</option>
-              <option value="2">Mujeres</option>
+              <option value="Male">Hombre</option>
+              <option value="Female">Mujeres</option>
             </select>
 
-            <select defaultValue={'DEFAULT'} class="bg-white border-2 border-black rounded py-2 px-4 placeholder-gray-500 text-black text-lg focus:bg-red-50 ">
+            <select name="position" defaultValue={"DEFAULT"} onChange={this.handleChange} class="bg-white border-2 border-black rounded py-2 px-4 placeholder-gray-500 text-black text-lg focus:bg-red-50 ">
               <option value="DEFAULT" disabled defaultValue>Posición: </option>
-              <option value="1">QB</option>
-              <option value="2">RB</option>
-              <option value="3">LB</option>
-              <option value="4">WR</option>
-              <option value="5">S</option>
-              <option value="6">CB</option>
-              <option value="7">C</option>
+              <option value="QB">QB</option>
+              <option value="RB">RB</option>
+              <option value="LB">LB</option>
+              <option value="WR">WR</option>
+              <option value="S">S</option>
+              <option value="CB">CB</option>
+              <option value="C">C</option>
             </select>
 
             <label class="bg-white border-2 border-black rounded py-2 px-4 m-0 p-0 ">
@@ -80,7 +92,7 @@ export default class registerPlayer extends Component {
         <div class="flex justify-end">
           <div class="ml-20">
             <Link to="/dashboard">
-              <button type="button" class=" bg-black text-white rounded-full px-4 py-3 mt-10">
+              <button type="button" onClick={() => this.postPlayers} class=" bg-black text-white rounded-full px-4 py-3 mt-10">
                 <label class="ml-4 text-lg">Registrar</label>
                 <FontAwesomeIcon className="flex-1 ml-5 mr-1" icon={["fas", "user-check"]} size="1x" />
               </button>
