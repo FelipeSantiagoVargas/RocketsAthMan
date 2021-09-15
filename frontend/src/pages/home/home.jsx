f44d0ef565e5c166736b4e2870d6766ab381df7d
eithri <guillermo.paez@uptc.edu.co>
2021-09-15 02:40:39 -0500

    added carrousel

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../../components/header";
+import Carrousel from "../../components/carrousel";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -58,6 +59,7 @@ export default function Home() {
           </p>
         </section>
       </div>
+      <Carrousel />
     </div>
   );
 }
